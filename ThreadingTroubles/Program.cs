3eb3b5f89a3f3e7279af624348af60305816b62f
Infarh <shmachilin@yandex.ru>
2020-05-21 12:37:23 +0300

    Демонстрация проблем с доступом потоков к критической секции - консоли

--- ThreadingTroubles/Program.cs
+++ ThreadingTroubles/Program.cs
@@ -15,40 +15,65 @@ namespace ThreadingTroubles
 
         static void Main(string[] args)
         {
-            var data_directory = new DirectoryInfo(data_dir);
+            //var data_directory = new DirectoryInfo(data_dir);
 
-            var words_count = 0;
-            foreach (var file in data_directory.GetFiles()) // 468 152
+            //var words_count = 0;
+            //foreach (var file in data_directory.GetFiles()) // 468 152
+            //{
+            //    Console.WriteLine("{0} - {1}kb", file.Name, file.Length / 1024.0);
+
+            //    words_count += GetWordsCount(file);
+            //}
+
+            //Console.WriteLine("Число слов {0} - последовательно", words_count);
+
+
+            //var files = data_directory.GetFiles();
+
+            //var threads = new Thread[files.Length];
+            //words_count = 0;
+            //for (var i = 0; i < threads.Length; i++) //127 912
+            //{
+            //    var file_to_process = files[i];
+            //    threads[i] = new Thread(() => words_count = words_count + GetWordsCount(file_to_process));
+            //    threads[i].Start();
+            //}
+
+            //for (var i = 0; i < threads.Length; i++)
+            //    threads[i].Join();
+
+            //Console.WriteLine("Число слов {0} параллельно", words_count);
+
+            var print_threads = new Thread[10];
+            for (var i = 0; i < print_threads.Length; i++)
             {
-                Console.WriteLine("{0} - {1}kb", file.Name, file.Length / 1024.0);
-
-                words_count += GetWordsCount(file);
+                var message = $"Thread {i + 1} message";
+                var thread = new Thread(() => PrintConsoleData(message, 10, 0));
+                thread.Start();
+                print_threads[i] = thread;
             }
-
-            Console.WriteLine("Число слов {0} - последовательно", words_count);
-
-
-            var files = data_directory.GetFiles();
-
-            var threads = new Thread[files.Length];
-            words_count = 0;
-            for (var i = 0; i < threads.Length; i++) //127 912
-            {
-                var file_to_process = files[i];
-                threads[i] = new Thread(() => words_count = words_count + GetWordsCount(file_to_process));
-                threads[i].Start();
-            }
-
-
-            for (var i = 0; i < threads.Length; i++)
-                threads[i].Join();
-
-            Console.WriteLine("Число слов {0} параллельно", words_count);
 
             Console.ReadLine();
         }
 
         private static readonly char[] __Separators = { ' ', '.', ',', '!', ';', '-', '(', ')', '[', ']', '{', '}' };
+
+        private static void PrintConsoleData(string Message, int Count, int Timeout = 10)
+        {
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Поток {0} запущен", thread_id);
+
+            for (var i = 0; i < Count; i++)
+            {
+                Console.Write("{0,4}:", i);
+                Console.Write(Message);
+                Console.WriteLine(" - thread id:{0}", thread_id);
+
+                Thread.Sleep(Timeout);
+            }
+
+            Console.WriteLine("Поток {0} завершён", thread_id);
+        }
 
         private static int GetWordsCount(FileInfo file)
         {
